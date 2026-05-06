9b57935c441b83f32d1f57aa3a704edd6c8b756e
ZeroK <149679147+SSJ-05@users.noreply.github.com>
2026-05-06 22:06:59 +0530

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -17,6 +17,44 @@
 int main () {
     std::printf("\n\n");
 
+    constexpr std::string_view  MY_PORT   { "5555" };   // client will connect to this port
+    constexpr std::uint8_t      BACKLOG   { 10 };       // no. of requests to connect to be queued
+
+    // create struct addrinfo
+    sockaddr_storage cli_addr;
+    socklen_t addr_size;
+    addrinfo hints, *res;
+    int socketfd, new_fd;
+
+    std::memset(&hints, 0, sizeof(hints));
+    hints.ai_family     =   AF_INET;
+    hints.ai_socktype   =   SOCK_STREAM;
+    hints.ai_flags      =   AI_PASSIVE;
+
+    getaddrinfo (nullptr, MY_PORT, &hints, &res);
+
+    // 1. create a socket
+    socketfd = socket (res->ai_family, res->ai_socktype, res->ai_protocol);
+
+
+    // 2. bind to port we passed in getaddrinfo
+    bind (socketfd, res->ai_addr, res->ai_addrlen);
+    // connect (socketfd, res->ai_addr, res->ai_addrlen);
+
+    int yes { 1 };
+    setsockopt (listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+
+
+    // 3. listen for incoming requests from client
+    
+
+    // 4. accept request - TCP handshake
+
+
+    // 5. send
+
+
+    // 6. recv
 
 
 
