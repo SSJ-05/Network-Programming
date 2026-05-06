4477af64042fbe3985070065fc9c74fbd2db464c
ZeroK <149679147+SSJ-05@users.noreply.github.com>
2026-05-06 20:35:40 +0530

    Add files via upload

---
+++ server.cpp
@@ -0,0 +1,26 @@
+// Echo server// 06.05.26// ZeroK
+// client sends 'hello' -> server sends back 'hello'
+// server flow : socket -> bind -> listen -> accept -> send/recv
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstdlib>
+#include <cstdint>
+#include <cstddef>
+#include <cstdio>
+#include <cstring>
+
+
+int main () {
+    std::printf("\n\n");
+
+
+
+
+
+    std::printf("\n\n");
+    return EXIT_SUCCESS;
+}
